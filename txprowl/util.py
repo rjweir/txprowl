a001c87980388c34eb42bcaec391076b8cde2b74
Rob Weir <rweir@ertius.org>
2009-12-20 22:08:12 +1100

    Add docstring.

--- txprowl/util.py
+++ txprowl/util.py
@@ -1,4 +1,5 @@
 def output_result(args):
+    """Nicely format and print the result of a successful API call."""
     remaining, reset_date = args
     print "%d API calls remaining - counter will reset at %s" % (remaining, reset_date.strftime("%H:%M on %Y%m%d"))
     return remaining, reset_date
